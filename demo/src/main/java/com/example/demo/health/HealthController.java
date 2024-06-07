0988fa5ef1ff8c550acbb30a54cac80f30c35e56
Leonardo Mejía <leomejia646@gmail.com>
2024-06-07 17:20:45 -0600

    Agregar swagger y modificaciones para la seguridad con swagger

--- demo/src/main/java/com/example/demo/health/HealthController.java
+++ demo/src/main/java/com/example/demo/health/HealthController.java
@@ -1,5 +1,7 @@
 package com.example.demo.health;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -7,17 +9,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/health")
 @Api(tags = "Health")
+@Tag(name = "Health API", description = "Verifica el estado de la API")
 public class HealthController {
 
     @GetMapping("/check")
-    @ApiOperation("Verifica el estado de la API")
-    public ResponseEntity<String> checkAPI(){
+    @Operation(summary = "Verifica el estado de la API")
+    public ResponseEntity<String> checkAPI() {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body("<h1>The API is working ok!</h1>");
--- demo/src/main/java/com/example/demo/security/SecurityConfiguration.java
+++ demo/src/main/java/com/example/demo/security/SecurityConfiguration.java
@@ -17,7 +17,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfiguration {
@@ -57,7 +56,15 @@ public class SecurityConfiguration {
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(
+                                "/api/auth/**",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/v3/api-docs.yaml",
+                                "/swagger-ui.html",
+                                "/webjars/**",
+                                "/health/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 );
 
